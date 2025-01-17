646a93fc095facf58d3ea068f46241cfbef9410b
Kun Duan <dklospace@gmail.com>
2025-01-16 16:36:25 -0800

    wip

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from PIL import Image
 
 from model import VAE
 
+
 def collate_mnist_fn(data: list[tuple[Image, int]]) -> tuple[torch.Tensor, torch.Tensor]:
     images = torch.zeros((0, 1, 28, 28), dtype=torch.float32)  # BxCxHxW, where H=28, W=28, C=1 for mnist
     labels = torch.zeros((0,), dtype=torch.int64)  # Bx1
@@ -33,24 +34,29 @@ def main():
     writer = SummaryWriter(log_dir=f"runs/{log_name}")
     dataset = torchvision.datasets.MNIST("./", train=True, download=True)
     dataloader = DataLoader(dataset=dataset, batch_size=64, shuffle=True, collate_fn=collate_mnist_fn)
-    vae = VAE(input_size=784, z_dim=64)
-    optimizer = torch.optim.AdamW(params=vae.parameters(), lr=1e-3)
+    vae = VAE(input_size=784, z_dim=32, dropout=0)
+    optimizer = torch.optim.AdamW(params=vae.parameters(), lr=1e-3, weight_decay=1e-4)
+    n_epochs = 10
 
     i = 0
-    for images, _ in dataloader:
-        optimizer.zero_grad()
-        outputs, recon_loss, kl_loss = vae(images)
-        total_loss = recon_loss + kl_loss
-        total_loss.backward()
-        torch.nn.utils.clip_grad_norm_(vae.parameters(), max_norm=1.0)
-        optimizer.step()
-        writer.add_images("train/images", images, i)
-        writer.add_images("train/outputs", outputs, i)
-        writer.add_scalar("train/recon_loss", recon_loss.item(), i)
-        writer.add_scalar("train/kl_loss", kl_loss.item(), i)
-        writer.add_scalar("train/total_loss", total_loss.item(), i)
-        writer.add_scalar("train/grad_norm", grad_norm(vae.parameters()), i)
-        i += 1
+    for _ in range(n_epochs):
+        for x, _ in dataloader:
+            optimizer.zero_grad()
+            x_recon, recon_loss, kl_loss, total_loss, p_mu, p_log_sigma_2, q_mu, q_log_sigma_2 = vae(x)
+            total_loss.backward()
+            # torch.nn.utils.clip_grad_norm_(vae.parameters(), max_norm=1.0)
+            optimizer.step()
+            writer.add_images("train/images/x", x, i)
+            writer.add_images("train/images/x_recon", x_recon, i)
+            writer.add_scalar("train/loss/recon", recon_loss.item(), i)
+            writer.add_scalar("train/loss/kl", kl_loss.item(), i)
+            writer.add_scalar("train/loss/total", total_loss.item(), i)
+            writer.add_scalar("train/model/grad_norm", grad_norm(vae.parameters()), i)
+            writer.add_scalar("train/p/mu", torch.mean(p_mu), i)
+            writer.add_scalar("train/p/log_sigma_2", torch.mean(p_log_sigma_2), i)
+            writer.add_scalar("train/q/mu", torch.mean(q_mu), i)
+            writer.add_scalar("train/q/log_sigma_2", torch.mean(q_log_sigma_2), i)
+            i += 1
 
 if __name__ == "__main__":
     main()
--- model.py
+++ model.py
@@ -2,76 +2,81 @@ import torch
 from torch import nn
 
 
-class Encoder(nn.Module):
-    def __init__(self, input_size: int, z_dim):
+class Block(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int):
         super().__init__()
-        self._linear1 = nn.Linear(input_size, input_size // 2)
-        self._linear2 = nn.Linear(input_size // 2, input_size // 4)
-        self._mu = nn.Linear(input_size // 4, z_dim) # \mu
-        self._log_sigma2 = nn.Linear(input_size // 4, z_dim) # log(\simga^2)
+        self._linear = nn.Linear(in_dim, out_dim)
+        self._ln = nn.LayerNorm(out_dim)
 
-    def forward(self, inputs: torch.Tensor):
-        outputs = self._linear1(inputs)
-        outputs = nn.functional.relu(outputs)
-        outputs = self._linear2(outputs)
-        outputs = nn.functional.relu(outputs)
-        return self._mu(outputs), self._log_sigma2(outputs)
+    def forward(self, x):
+        x = self._linear(x)
+        x = self._ln(x)
+        return nn.functional.relu(x)
 
+class FFN(nn.Module):
+    def __init__(self, dims: list[int], dropout: float):
+        super().__init__()
+        self._blocks = nn.ModuleList([Block(in_dim, out_dim) for in_dim, out_dim in zip(dims[:-1], dims[1:])])
+        self._dropout = nn.Dropout(p=dropout)
+
+    def forward(self, x):
+        for block in self._blocks:
+            x = block(x)
+        return self._dropout(x)
+
+class Encoder(nn.Module):
+    def __init__(self, input_size: int, z_dim: int, dropout: float):
+        super().__init__()
+        # self._ffn = FFN(dims=[input_size, z_dim * 16, z_dim * 4], dropout=dropout)
+        self._mu = nn.Linear(input_size, z_dim) # \mu
+        self._log_sigma2 = nn.Linear(input_size, z_dim) # log(\simga^2)
+
+    def forward(self, x: torch.Tensor):
+        # x = self._ffn(x)
+        return self._mu(x), torch.clamp(self._log_sigma2(x), min=-1.5, max=1.5)
 
 class Decoder(nn.Module):
-    def __init__(self, output_size: int, z_dim):
+    def __init__(self, output_size: int, z_dim: int, dropout: float):
         super().__init__()
-        self._linear1 = nn.Linear(z_dim, z_dim * 2)
-        self._linear2 = nn.Linear(z_dim * 2, z_dim * 4)
-        self._mu = nn.Linear(z_dim * 4, output_size)
-        self._log_sigma2 = nn.Linear(z_dim * 4, output_size)
+        # self._ffn = FFN(dims=[z_dim, z_dim * 4, z_dim * 16], dropout=dropout)
+        self._mu = nn.Linear(z_dim, output_size)
+        self._log_sigma2 = nn.Linear(z_dim, output_size)
 
-    def forward(self, inputs: torch.Tensor):
-        outputs = self._linear1(inputs)  # Bxz_dim -> Bx(z_dim*2)
-        outputs = nn.functional.relu(outputs)
-        outputs = self._linear2(outputs)
-        outputs = nn.functional.relu(outputs)
-        return self._mu(outputs), self._log_sigma2(outputs)
+    def forward(self, z: torch.Tensor):
+        # z = self._ffn(z)
+        return self._mu(z), torch.clamp(self._log_sigma2(z), min=-1.5, max=1.5)
 
 class VAE(nn.Module):
-    def __init__(self, input_size: int, z_dim: int = 64):
+    def __init__(self, input_size: int, z_dim: int = 16, dropout: float = 0.1):
         super().__init__()
-        self.encoder = Encoder(input_size=input_size, z_dim=z_dim)
-        self.decoder = Decoder(output_size=input_size, z_dim=z_dim)
+        self.encoder = Encoder(input_size=input_size, z_dim=z_dim, dropout=dropout)
+        self.decoder = Decoder(output_size=input_size, z_dim=z_dim, dropout=dropout)
         self._input_size = input_size
         self._z_dim = z_dim
 
-    def compute_loss(self, inputs: torch.Tensor, outputs: torch.Tensor,
-                     z_mu: torch.Tensor, z_log_sigma_2: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        recon_loss = torch.nn.functional.mse_loss(inputs, outputs)
-        kl_loss = -torch.mean(torch.sum(1 + z_log_sigma_2 - z_mu ** 2 - torch.exp(z_log_sigma_2), dim=-1))
-        return recon_loss, kl_loss
+    def compute_loss(self, x: torch.Tensor, p_mu: torch.Tensor, p_log_sigma_2: torch.Tensor, z_mu: torch.Tensor, z_log_sigma_2: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        # Loss function is negative ELBO and contain two parts
+        # 1) recon_loss: -log(p(x|z)).
+        # In gaussian case (p(x|z)), this is the negative log likelihood of a gaussian, rather than MSE
+        # Using MSE between (inputs, outputs) pairs is minimizing |x-x_recon|^2, which effectively eliminating
+        # the variance term and thus only favoring the means. This cause very blurry, mean-image like, reconstructions.
+        recon_loss = torch.mean(0.5 * torch.sum(p_log_sigma_2, dim=-1) + 0.5 * torch.sum((x - p_mu) ** 2 / torch.exp(p_log_sigma_2), dim=-1))
+        # 2) kl_loss: KL(q(z|x) || p(z)), where the prior p(z) is assumed to be unit gaussian
+        kl_loss = torch.mean(0.5 * (torch.sum(torch.exp(z_log_sigma_2) - 1 - z_log_sigma_2, dim=-1) + torch.sum(z_mu ** 2, dim=-1)))
+        # total_loss: sum of the two loss terms
+        total_loss = recon_loss + 0.1 * kl_loss
+        return recon_loss, kl_loss, total_loss
 
-    def forward(self, inputs: torch.Tensor):
+    def forward(self, x: torch.Tensor):
         # inputs: BxCxHxW
-        inputs = inputs.view(inputs.size(0), -1)  # BxCxHxW -> Bx1
+        x = x.view(x.size(0), -1)  # BxCxHxW -> Bx1
+        batch_size = x.size(0)
         # The forward pass of the VAE takes a real sample as input,
         # then generates its reconstructed version
-        z_mu, z_log_sigma_2 = self.encoder(inputs)
-        # Sample z from unit gaussian
-        q = torch.distributions.MultivariateNormal(
-            torch.zeros(inputs.size(0), self._z_dim),
-            torch.eye(self._z_dim).repeat(inputs.size(0), 1, 1))
-        z = q.sample()
-        x_mu, x_log_sigma_2 = self.decoder(z * torch.sqrt(torch.exp(z_log_sigma_2)) + z_mu)
-        # Sample x from unit gaussian
-        p = torch.distributions.MultivariateNormal(
-            torch.zeros(inputs.size(0), self._input_size),
-            torch.eye(self._input_size).repeat(inputs.size(0), 1, 1))
-        x = p.sample()
-        outputs = x * torch.sqrt(torch.exp(x_log_sigma_2)) + x_mu
-        recon_loss, kl_loss = self.compute_loss(inputs, outputs, z_mu, z_log_sigma_2)
-        return outputs.view(inputs.size(0), 1, 28, 28), recon_loss, kl_loss
-
-    @torch.no_grad()
-    def encode(self, inputs: torch.Tensor):
-        return self.encoder(inputs)
-
-    @torch.no_grad()
-    def decode(self, inputs: torch.Tensor):
-        return self.decoder(inputs)
+        q_mu, q_log_sigma_2 = self.encoder(x)
+        # Sample z from q(z|x)
+        z = torch.randn_like(q_mu) * torch.exp(0.5 * q_log_sigma_2) + q_mu
+        p_mu, p_log_sigma_2 = self.decoder(z)
+        x = torch.randn_like(p_mu) * torch.exp(0.5 * p_log_sigma_2) + p_mu
+        recon_loss, kl_loss, total_loss = self.compute_loss(x, p_mu, p_log_sigma_2, q_mu, q_log_sigma_2)
+        return x.view(batch_size, 1, 28, 28), recon_loss, kl_loss, total_loss, p_mu, p_log_sigma_2, q_mu, q_log_sigma_2
